4ac1589e8d4408db30332157b86b26fe297dc650
sans-algo <sanskar.pawar2003@gmail.com>
2026-01-15 15:24:31 +0530

    update backend config due to sercurity issue

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+
+    console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error("MongoDB connection failed");
     process.exit(1);
   }
 };
